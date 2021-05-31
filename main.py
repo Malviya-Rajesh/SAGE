071cf4f5a5f9244cb42d63f5b6ba5906638df102
Rajesh Malviya <84065620+Malviya-Rajesh@users.noreply.github.com>
2021-05-31 10:50:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 """
 AI voice assistant "SAGE"
-his program is a AI voice assistant which is use to play movies and search on google and
+Its program is a AI voice assistant which is use to play movies and search on google and
 open youtube some basic question to ai like his name and age etc.
 """
 
